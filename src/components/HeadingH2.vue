af93f06b6382c21ae24a98ae650454c5c3437019
Siddhesh Mangela <sidthecool007@gmail.com>
2021-03-16 12:44:17 +0530

    layout init

---
+++ src/components/HeadingH2.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2 class="text-xl md:text-3xl font-medium">
+    <slot />
+  </h2>
+</template>
---
+++ src/components/SectionBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <article class="section-box">
+    <section class="section-box__heading">
+      <HeadingH2>{{ heading }}</HeadingH2>
+    </section>
+    <section class="section-box__body">
+      <slot />
+    </section>
+  </article>
+</template>
+
+<script setup>
+import HeadingH2 from './HeadingH2.vue';
+import { defineProps } from 'vue';
+
+defineProps({
+  heading: String,
+});
+</script>
